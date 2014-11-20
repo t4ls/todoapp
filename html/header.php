885658475af2aa3cb3afac7bb154271792b333b3
Cade Gillem <cpgillem@gmail.com>
2014-11-19 23:06:34 -0500

    Create basic working webpage.

---
+++ html/header.php
@@ -0,0 +1,7 @@
+<?php
+    require_once 'core.php';
+?>
+<div class="header">
+    <div class="title" id="title"><h1><?php echo TITLE; ?></h1></div>
+    <div class="title" id="date"><h1><?php echo get_date(); ?></h1></div>
+</div>
---
+++ html/index.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<?php
+    require_once 'core.php'
+?>
+<html><div class="container">
+    <head>
+        <title><?php echo TITLE; ?></title>
+        <link rel="stylesheet" href="layout.css"/>
+        <link rel="stylesheet" href="style.css"/>
+    </head>
+    <body>
+        <?php include_once 'header.php'; ?>
+    </body>
+</div></html>
