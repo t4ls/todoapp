1268480b9ff6af8b41940a0234a4ff3a9c4f0fd4
Cade Gillem <cpgillem@gmail.com>
2014-11-23 15:13:13 -0500

    Add functions to db_interface.

--- html/db_interface.php
+++ html/db_interface.php
@@ -6,10 +6,18 @@
 
     $db = new mysqli(HOST, USER, PASSWORD, DATABASE);
 
+    function enforce_string_length($string, $length) {
+        if (strlen($string) > $length) {
+            return $string = substr($string, 0, $length);
+        }
+    }
+
     function add_user($username) {
     	global $db;
 
-    	$add_user = $db->prepare("INSERT INTO users (username)
+        enforce_string_length($username, 24);
+
+    	$add_user = $db->prepare("INSERT INTO users(username)
     							  VALUES (?)");
 
     	$add_user->bind_attrib('s', $username);
@@ -17,5 +25,22 @@
     	$add_user->close();
     }
 
+    function add_task($description) {
+        global $db;
 
+        enforce_string_length($description, 200);
+
+        $add_task = $db->prepare("INSERT INTO tasks(description)
+                                  VALUES (?)");
+
+        $add_task->bind_attrib('s', $description);
+        $add_task->execute();
+        $add_task->close();
+    }
+
+    function add_location($name) {
+        global $db;
+
+        enforce_string_length($name, 100);
+    }
  ?>
