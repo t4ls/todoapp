6fe0c708d627aded1a61f7b03abe25a56cca95c2
Cade Gillem <cpgillem@gmail.com>
2014-11-25 15:30:59 -0500

    Fix column layout problems.

--- html/list.php
+++ html/list.php
@@ -1,23 +1,27 @@
 <div class="section" id="list" >
 	<div class="column" id="task_list">
-		<ol class="list" id="tasks">
+		<div class="list" id="tasks">
 			<?php 
-			// TODO: populate task list with items with checkboxes.
-			// Example on next HTML line. 
+			// TODO: populate task divst with items with checkboxes.
+			// Example on next HTML divne. 
 			?>
-			<li class="task" id="task_0">
+			<div class="task" id="task_0">
 				<input class="checkbox" type="checkbox" name="task_0_selected"/>
 				<div class="task_description" id="task_0_description">the description</div>
-			</li>
-			<li class="task" id="task_1">
+			</div>
+			<div class="task" id="task_1">
 				<input class="checkbox" type="checkbox" name="task_1_selected"/>
 				<div class="task_description" id="task_1_description">the description</div>
-			</li>
-			<li class="task" id="task_2">
+			</div>
+			<div class="task" id="task_2">
 				<input class="checkbox" type="checkbox" name="task_2_selected"/>
 				<div class="task_description" id="task_2_description">the description</div>
-			</li>
-		</ol>
+			</div>
+			<div class="task" id="task_2">
+				<input class="checkbox" type="checkbox" name="task_2_selected"/>
+				<div class="task_description" id="task_2_description">the description</div>
+			</div>
+		</div>
 	</div>
 	<div class="column" id="task_details">
 		<?php
