225d46afa23ce8ab4fab8c73b3050a722158a6a5
blake <Blakewrege1@gmail.com>
2014-11-30 05:46:47 -0500

    adding first and last name columns

--- html/login.php
+++ html/login.php
@@ -3,7 +3,9 @@
 
 <br><br>
 <form action="usersql.php" method="post">
-Name: <input type="text" name="name"><br>
+username: <input type="text" name="username"><br>
+first name: <input type="text" name="first_name"><br>
+last name: <input type="text" name="last_name"><br>
 E-mail: <input type="text" name="email"><br>
 <input type="submit">
 </form>
