1f3ac46be0db7af051eb4695bbc36515d4defff8
blake <Blakewrege1@gmail.com>
2014-11-30 05:06:41 -0500

    for adding users

---
+++ html/login.php
@@ -0,0 +1,14 @@
+<title>Add User</title>
+<html><body><h1>Add User</h1>
+
+<br><br>
+<form action="usersql.php" method="post">
+Name: <input type="text" name="name"><br>
+E-mail: <input type="text" name="email"><br>
+<input type="submit">
+</form>
+
+<li><a href="http://t4ls.duckdns.org:8080/readsql.php">read users table</a></li>
+<li><a href="http://t4ls.duckdns.org:8080/clearsql.php">clear users table</a></li>
+<li><a href="http://t4ls.duckdns.org:8080/todoapp/html">Main Menu</a></li>
+</body></html>
