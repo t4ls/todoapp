c72991e86cd4ebfff52348d986b349fb2baada1e
gigglesbw4 <blakewrege1@gmail.com>
2014-12-01 14:24:28 -0500

    login page now checks if users exist and will not allow new users with the same username

--- html/newtask/login.php
+++ html/newtask/login.php
@@ -1,33 +1,104 @@
 <!DOCTYPE html>
+
+<?php
+	
+	$servername = "localhost";
+	$username = "root";
+	$password = "monkey";
+	$dbname = "t4ls_todo";
+	$conn = new mysqli($servername, $username, $password, $dbname);
+	$arr = array();
+	$ii = 0;
+	$srows = mysqli_query($conn,"SELECT username FROM users");
+	if ($srows->num_rows > 0) {
+	        while($row = $srows->fetch_assoc()) {
+	        $arr[$ii] = $row["username"];
+	        $ii++;
+	    	}
+	}
+	else {
+	        echo "0 results, no users exist\n";
+	}	
+echo "Current number of users: ".count($arr);
+$conn->close();
+?>
+
+
+
+
+
+
+
 <html>
 <head>
-<p> Login or create a new user </p>
 <br>
-
-
 <script>
-function validateForm() {
-    var x = document.forms["myForm"]["username"].value;
+function validateLogin() {
+    var x = document.forms["login"]["username"].value;
     if (x == "" || x==null) {
         alert("A username is required");
         return false;
     }
+	var users = <?php echo json_encode($arr); ?>;
+	for (jj=0; jj < users.length; jj++){
+		if (users[jj]==x) {
+		return true;
+		}
+	}
+	alert("The username does not exist");
+    return false;
+}
+
+
+function validateNew() {
+    var x = document.forms["newuser"]["username"].value;
+    if (x == "" || x==null) {
+        alert("A username is required");
+        return false;
+    }
+    
+    	var users = <?php echo json_encode($arr); ?>;
+		for (jj=0; jj < users.length; jj++){
+		if (users[jj]==x) {
+		alert("That username already exists");
+		return false;
+		}
+	}    
+
+    var x = document.forms["newuser"]["email"].value;
+    var atpos = x.indexOf("@");
+    var dotpos = x.lastIndexOf(".");
+    if (atpos< 1 || dotpos<atpos+2 || dotpos+2>=x.length) {
+    	alert("Not a valid e-mail address");
+    	return false;
+    }
+    
+    var x = document.forms["newuser"]["first_name"].value;
+    if (x == "" || x==null) {
+        alert("A first name is required");
+        return false;
+    }
+    
+    var x = document.forms["newuser"]["last_name"].value;
+    if (x == "" || x==null) {
+        alert("A last name is required");
+        return false;
+    }    
+
 }
 </script>
-
 </head>
 
 <body>
-
 <h3>Login</h3>
-<form name="myForm" form action="welcome.php" onsubmit="return validateForm();" method="post">
+<form name="login" form action="welcome.php" onsubmit="return validateLogin();" method="post">
 username: <input type="text" name="username"><br>
 <input type="submit" value="Submit">
 </form>
 <br><br>
 
-<h3>New User</h3>
-<form action="newuser.php" onsubmit="return validateForm();" method="post">
+<h3>Create New User</h3>
+<form name="newuser" form action="newuser.php" onsubmit="return validateNew();" method="post">
 username: <input type="text" name="username"><br>
 first name: <input type="text" name="first_name"><br>
 last name: <input type="text" name="last_name"><br>
