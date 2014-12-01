56067da42c39bca6259af98d875642396ebb30e7
gigglesbw4 <blakewrege1@gmail.com>
2014-12-01 10:41:50 -0500

    added script to check for empty username

--- html/newtask/login.php
+++ html/newtask/login.php
@@ -1,22 +1,38 @@
-<html><body>
+<!DOCTYPE html>
+<html>
+<head>
 <p> Login or create a new user </p>
 <br>
 
 
+<script>
+function validateForm() {
+    var x = document.forms["myForm"]["username"].value;
+    if (x == "" || x==null) {
+        alert("A username is required");
+        return false;
+    }
+}
+</script>
+
+</head>
+
+<body>
+
 <h3>Login</h3>
-<form action="welcome.php" method="post">
+<form name="myForm" form action="welcome.php" onsubmit="return validateForm();" method="post">
 username: <input type="text" name="username"><br>
-<input type="submit">
+<input type="submit" value="Submit">
 </form>
 <br><br>
 
 <h3>New User</h3>
-<form action="newuser.php" method="post">
+<form action="newuser.php" onsubmit="return validateForm();" method="post">
 username: <input type="text" name="username"><br>
 first name: <input type="text" name="first_name"><br>
 last name: <input type="text" name="last_name"><br>
 E-mail: <input type="text" name="email"><br>
-<input type="submit">
+<input type="submit" value="Submit">
 
 </form>
 </body></html>
